ae35e2d0ad3d658e55ad11ff80d7432e44de222c
boaduajennifer <beekellyb77@gmail.com>
2024-10-17 20:15:12 +0000

    update and installing packages

---
+++ Bash_Scripting/install_packages.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#update the packages
+sudo apt update
+#install fortune
+sudo apt install -y fortune
+#install cowsay
+sudo apt install -y cowsay
+#install cmatrix
+sudo apt install -y cmatrix
+chmod +x install_packages.sh   
