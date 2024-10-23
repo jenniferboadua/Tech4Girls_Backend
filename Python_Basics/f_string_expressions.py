c0f5e165a9b86a65d5fd6b2bf14febe5e144a602
boaduajennifer <beekellyb77@gmail.com>
2024-10-23 10:19:20 +0000

    string expression

---
+++ Python_Basics/f_string_expressions.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+a= '10'
+b= '20'
+print(f'the sum of {a} and {b} is {a+b}.')
+print(f'the difference between {a} and {b} is {a-b}.')
+print(f'the product of {a} and {b} is {a*b}.')
