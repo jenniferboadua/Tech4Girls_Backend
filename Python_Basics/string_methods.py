b58c8ef6f0b0ba8a87ae37eab58ee7e02934422f
boaduajennifer <beekellyb77@gmail.com>
2024-10-23 10:05:27 +0000

    strings

---
+++ Python_Basics/string_methods.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+#script to demonstrate some strings
+message='Hello dear,can you cancel class?'
+
+#demonstrating uppercase
+print(message.upper())
+
+#demonstrating lowercase
+print(message.lower())
+
+#demonstrating replace
+print(message.replace('cancel', 'go'))
+
+#demonstrating join
+name= 'Grace'
+directive= 'come here'
+print(name + '' + directive)
