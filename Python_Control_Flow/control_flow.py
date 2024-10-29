f50652f6f8b0f1202cd7212ae0905b55104d0b26
boaduajennifer <beekellyb77@gmail.com>
2024-10-29 22:25:59 +0000

    control flow

---
+++ Python_Control_Flow/control_flow.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+age='what is your age'
+if age < 18:
+    print("You are a minor")
+elif 18 <= age <= 64:
+    print("You are an adult")
+elif age >= 65:
+    print("You are a senior")
