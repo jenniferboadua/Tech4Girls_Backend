38a88743ae0e48013fdf6114e951c37390d1444c
boaduajennifer <beekellyb77@gmail.com>
2024-10-29 21:55:04 +0000

    tuple

---
+++ Python_Control_Flow/tuple_and_type_conversion.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+number_tuple=(1, 6, 7, 8, 9)
+
+#to print first and last number using indexing
+number_tuple.index(0)
+print(number_tuple)
+number_tuple.index(4)
+print(number_tuple)
+
+#demonstrating the use of count and index
+number_tuple.index(3)
+number_tuple.count(3)
+
+#conversion
+a=int(3.8)
+print(a)
+b=float(7)
+print(b)
+c=int()
+print(c)
+
+name='Grace'
+action='come here'
+combine=f'{name}{action}'
+print(combine)
+
