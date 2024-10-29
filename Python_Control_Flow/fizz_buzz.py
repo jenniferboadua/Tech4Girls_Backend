47925e2f100824d4b987200175c5d6443855f339
boaduajennifer <beekellyb77@gmail.com>
2024-10-29 22:59:38 +0000

    fizz

---
+++ Python_Control_Flow/fizz_buzz.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+for number in range(1,51):
+    if number % 3 == 0 and number % 5 == 0:
+        print("Fizzbuzz")
+    elif number % 3 ==0:
+        print("Fizz")
+    elif number % 5 ==0:
+        print("Buzz")
+    else:
+        print(number)
