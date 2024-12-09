58597132b16a990fafd4e7f92a1439a8adf9da25
boaduajennifer <beekellyb77@gmail.com>
2024-12-09 20:50:16 +0000

    oop

--- OOP_Concepts/class_basics.py
+++ OOP_Concepts/class_basics.py
@@ -4,3 +4,11 @@ class Car:
         self.make = make
         self.model = model
         self.year = year
+    def display_info(self):
+        # print the car details
+        print(f"Make:{self.make}")
+        print(f"Model:{self.model}")
+        print(f"Year:{self.year}")
+        #create an instance of the car
+        car_1= Car("Toyota", "Camry", "2024")
+        car_1.display_info()
